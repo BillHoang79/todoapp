5a9b8a4b83ea5945fe7853b842ab376dfe572f6c
BillHoang79 <billhoang79@gmail.com>
2017-08-02 17:51:10 -0700

    make it better

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://todo:todo@ds121483.mlab.com:21483/todoapp'); 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos'); 
 
 var Todo = mongoose.model('Todo', {
 	task: String,
